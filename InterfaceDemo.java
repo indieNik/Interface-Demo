bc44d716d3bcf6241f77665d78537cd25c49c4ab
Nikhil Patil <indieNik@users.noreply.github.com>
2015-09-14 17:25:49 +0530

    Create InterfaceDemo.java

---
+++ InterfaceDemo.java
@@ -0,0 +1,20 @@
+package com.psl.print;
+
+public class InterfaceDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Printable p = new Employee();
+		p.print();
+		
+		Katrina k = new Vehicle();
+		k.acting();
+		
+		System.out.println(p);
+		
+	}
+
+}
