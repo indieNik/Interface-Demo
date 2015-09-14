34c8052ea33305de957af2020e857c1308aa5438
Nikhil Patil <indieNik@users.noreply.github.com>
2015-09-14 17:26:22 +0530

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,17 @@
+package com.psl.print;
+
+public class Vehicle implements Printable, Katrina{
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("Print from Vehicle!");
+	}
+
+	@Override
+	public void acting() {
+		// TODO Auto-generated method stub
+		System.out.println("Acting from Vehicle!");
+	}
+
+}
